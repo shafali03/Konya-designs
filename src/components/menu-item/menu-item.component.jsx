8619c2a2f07a3896f83647214c36bd53c704524f
shafali03 <shaf03sm@gmail.com>
2020-03-19 10:24:56 +0000

    style: delete duplicate css

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,15 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => (
 
-  <div style={{
-    backgroundImage: `url(${imageUrl})`
-  }} 
+  <div 
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }} 
   className={`${size} menu-item`}
   >
+    <div className ='background-image' style={{
+      backgroundImage: `url(${imageUrl})`
+    }}  />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
