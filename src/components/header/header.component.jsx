fdb26a1cfca2828da8c88788ac6b487206227320
shafali03 <shaf03sm@gmail.com>
2020-03-22 08:19:39 +0000

    feat: Added logo top of home page

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+
+import './header.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+
+const Header = () => (
+  <div className='header'>
+    <Link className='logo-container' to="/">
+      <Logo className='logo' /> 
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+    </div>
+  </div>
+)
+
+export default Header
