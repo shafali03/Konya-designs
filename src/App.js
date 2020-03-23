dc5f95abb56eb93ecdb0913621ea802f1fdc3470
shafali03 <shaf03sm@gmail.com>
2020-03-23 10:39:44 +0000

    feat: Set up Google sign in authentication with firebase

--- src/App.js
+++ src/App.js
@@ -7,22 +7,47 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 
 
 
-function App() {
-  return (
-  <div>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={ HomePage } />
-      <Route path='/shop' component={ ShopPage } />
-      <Route path='/signin' component={ SignInAndSignUpPage } />
+class App extends React.Component {
+  constructor() {
+    super();
 
-    </Switch>
-  </div>
-  );
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
+    return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/shop' component={ ShopPage } />
+        <Route path='/signin' component={ SignInAndSignUpPage } />
+      </Switch>
+    </div>
+    );
+  }
 }
 
 export default App;
