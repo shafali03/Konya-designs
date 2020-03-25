e72da4e20ceeb466aa67a07ecf9ccf503bff5997
shafali03 <shaf03sm@gmail.com>
2020-03-25 17:21:53 +0000

    feat: Imported cart drop down to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/card-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 
@@ -33,6 +34,7 @@ const Header = ({ currentUser }) => (
        )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 )
 
