2b54a46519aee03a9444d52d90669ae4b01380dc
shafali03 <shaf03sm@gmail.com>
2020-03-26 21:41:41 +0000

    feat: Created cart-item component and connected cart dropdown with cartItems reducer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
