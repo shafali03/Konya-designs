9092d9900f3a571e392d80ee2087576fe2797cd5
shafali03 <shaf03sm@gmail.com>
2020-03-27 19:54:32 +0000

    refactor: Using user selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
@@ -15,13 +16,14 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 )
 
+
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
-})
+const mapStateToProps = createStructuredSelector ({
+  itemCount: selectCartItemsCount
+});
 
 export default connect(
   mapStateToProps,
