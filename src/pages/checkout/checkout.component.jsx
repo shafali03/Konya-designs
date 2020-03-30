187278df38055c6201b4c8527cd17cfd3812229d
shafali03 <shaf03sm@gmail.com>
2020-03-30 17:16:57 +0100

    feat: Added Stripe integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe.button.component'
 
 import {
   selectCartItems,
@@ -34,16 +35,21 @@ const CheckoutPage = ({cartItems, total}) => (
     {cartItems.map(cartItem => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
       ))}
-    <div className='total'>
-      <span>TOTAL: £{total}</span>
+    <div className='total'>TOTAL: £{total}</div>
+    <div className='test-warning'>
+    *** Please use following test credit card for payment ***
+    
+    <br />
+    4242 4242 4242 4242 -Exp: 09/20 -CVV: 123
     </div>
+    <StripeCheckoutButton price={total} />
+    
   </div>
 )
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal
-})
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
-
