3132fd9869cb275e0321cc56ef555c8c37828a32
shafali03 <shaf03sm@gmail.com>
2020-03-31 13:10:32 +0100

    feat: Uploading firebase and ready to deploy to heroku

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDRknHgvvI7iyQGvY7ZH8rTzCAp9ecQaMA",
+    authDomain: "konya-designs.firebaseapp.com",
+    databaseURL: "https://konya-designs.firebaseio.com",
+    projectId: "konya-designs",
+    storageBucket: "konya-designs.appspot.com",
+    messagingSenderId: "341726462670",
+    appId: "1:341726462670:web:0216a6bc9ec53a4d94069f",
+    measurementId: "G-S2LM46KC8M"
+  };
+
+  firebase.initializeApp(config);
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+       console.log('error creating user', error.message);
+      }
+    }
+
+    return userRef;
+  }
+
+  
+
+
+  export const auth =firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  
+  export default firebase;
