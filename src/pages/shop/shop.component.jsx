9d82eed61c562ec347d4e505af460fccedeb0955
shafali03 <shaf03sm@gmail.com>
2020-04-25 09:07:18 +0100

    Feat: transform our array into map and update the shop redux files to be able to update and store collections map object

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import CollectionsOverview from '../../components/collections-overview/collection-overview.component'
 import CollectionPage from '../collection/collection.component'
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+import { updateCollections } from '../../redux/shop/shop.actions'
+
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionsSnapshotToMap(snapshot)
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      updateCollections(collectionsMap)
     })
   }
   render() {
@@ -28,5 +33,9 @@ class ShopPage extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
 
-export default ShopPage;
+
+export default connect(null, mapDispatchToProps)(ShopPage)
